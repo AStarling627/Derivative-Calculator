83eda84b5a317d7dee9be05a0c3660c1119b586e
AStarling627 <115578116+AStarling627@users.noreply.github.com>
2022-11-13 17:56:04 +0000

    Update Derivative Calculator.py

--- Derivative Calculator.py
+++ Derivative Calculator.py
@@ -8,7 +8,10 @@ co = []                                                                # they ca
 for func in function:
     if func.count('^') == 0:  # checks for a caret, which thus checks if there is an exponent present
         if func[:func.index('x')] == '':
-            coefficient = 1  # if there is no coefficient it sets it = 1 and appends it to the list
+            coefficient = str(1)  # if there is no coefficient it sets it = 1 and appends it to the list
+            co.append(coefficient)
+        if func[:func.index('x')] == '-':
+            coefficient = str(-1)
             co.append(coefficient)
         else:
             coefficient = func[:func.index('x')]  # however if there is a coefficient it appends that to the list
@@ -19,7 +22,7 @@ for func in function:
                 power.append(pow_dne)  # appends it to the power list
     else:  # this is for when there is an exponent
         if func[:func.index('^') - 1] == '':  # this deals with when there is no coefficient and sets it = 1
-            coefficient = str(1)
+            coefficient = 1
             co.append(coefficient)  # appends this to the list of coefficients
         else:
             coefficient = func[:func.index('^') - 1]  # however here, this is for when there is a coefficient
@@ -38,18 +41,21 @@ for func in function:
                     pow_sing = func[func.index('^') + 1]  # this is for when the exponent is not in parentheses
                     power.append(pow_sing)
 
+print(co)
+print(power)
+
 d_final = []  # this is an empty list for the derivative of each separate function
 
 for y in range(len(power)):  # a for loop relative to the length of the power list is used to differentiate specific
     if int(power[y]) - 1 == 1:                                           # circumstances of exponents in derivatives
         derivative = f'{int(co[y]) * int(power[y])}x'  # this is for when the exponent of the derivative is 1
         d_final.append(derivative)
-    if int(power[y]) - 1 == 0:
+    elif int(power[y]) - 1 == 0:
         derivative = f'{int(co[y]) * int(power[y])}'  # this is for when the exponent of the derivative is 0, thus the x
         d_final.append(derivative)                                                                     # term disappears
     else:
         derivative = f'{int(co[y]) * int(power[y])}x^{int(power[y]) - 1}'  # and this is for when the exponent of the
-        d_final.append(derivative)                                             # derivative is not 0 or 1 (most cases) 
-
-print(*d_final, sep=' + ')  # This final line is to separate each derivative in the list and insert a '+' in between each
-                                                 # to ensure that the original format of the input function is maintained
+        d_final.append(derivative)                                             # derivative is not 0 or 1 (most cases)
+print(d_final)
+print(*d_final, sep=' + ')  # This final line is to separate each derivative in the list and insert a '+' in between
+# each to ensure that the original format of the input function is maintained
